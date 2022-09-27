48ef01bcf691a752d04c48e676fb0ce44ccd13ab
AyonC <e10901ayon@dpsrkp.net>
2022-09-27 21:13:41 +0530

    add icon & title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import './App.css'
 
@@ -12,6 +13,11 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className='App'>
+      <Helmet>
+        <meta charSet='utf-8'/>
+        <title>Ayon C</title>
+        <meta name='portfolio' content='portfolio'/>;
+      </Helmet>
       <Router>
         <Navbar/>
         <Routes>
