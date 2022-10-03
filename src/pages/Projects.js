56552aae7d84fd9fa3fc4d09041c3f2adb1c4c75
AyonC <e10901ayon@dpsrkp.net>
2022-10-03 14:15:11 +0530

    complete: projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,31 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowUpRightFromSquare as open } from '@fortawesome/free-solid-svg-icons'
+
+import projectData from '../data/projectsData'
 
 function Projects() {
+  const num = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
   return (
-    <div>Projects</div>
+    <div>
+      <div className="heading">
+        projects
+      </div>
+      <div className="cards">
+        {
+          projectData.map(data => (
+            <div className="card">
+              <img className='img' src={`/${num(1, 4)}.jpeg`} alt='banner' width='200' height='100'></img>
+              <div className='name'>{data.name}</div>
+              <a className='openlink' href={data.link} target="blank">Open <FontAwesomeIcon icon={open}/></a>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
