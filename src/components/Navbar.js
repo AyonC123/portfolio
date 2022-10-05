f8011178d67c1828b5d0f6a62215c82cd8f17bb7
AyonC <e10901ayon@dpsrkp.net>
2022-10-05 17:32:22 +0530

    imporve: mobile responsivness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 import './../App.css'
 
 function Navbar() { 
+  const [open, setOpen] = useState(false)
   const themeSwitcher = () => {
     if (typeof window !== "undefined") {
       document.body.classList.toggle('lightTheme');
@@ -19,21 +20,43 @@ function Navbar() {
 
   return (
     <div className='navbar'>
-        <Link to='/'>
+      <Link to='/'>
         <div className='title'>Ayon</div>
+      </Link>
+      <div className='links responsive'>
+        <Link to='/skills' className='link'> 
+          Skills
         </Link>
-        <div className='links'>
-            <Link to='/skills' className='link'> 
-                Skills
+        <Link to='/projects' className='link'> 
+          Projects
+        </Link>
+        <Link to='/contact' className='link'> 
+          Contact
+        </Link>
+      </div>
+      <div className='navbar mobile-nav'>
+        {!open ? <FontAwesomeIcon icon={faBars} className='hamburger' onClick={() => setOpen(!open)}/> : <FontAwesomeIcon icon={faXmark} className='hamburger' onClick={() => setOpen(!open)}/>}
+        {
+        open && 
+        <div className='mobile-items'>
+          <FontAwesomeIcon icon={faMoon} className='icon mobile-hidden' onClick={themeSwitcher}/>
+          <div className='links link-items'>
+            <Link to='/' className='link link-item'>
+              Home
             </Link>
-            <Link to='/projects' className='link'> 
-                Projects
+            <Link to='/skills' className='link link-item'> 
+              Skills
             </Link>
-            <Link to='/contact' className='link'> 
-                Contact
+            <Link to='/projects' className='link link-item'> 
+              Projects
             </Link>
+            <Link to='/contact' className='link link-item'> 
+              Contact
+            </Link>
+          </div>
         </div>
-       <FontAwesomeIcon icon={faMoon} className='icon' onClick={themeSwitcher}/> 
+        }
+      </div>
     </div>
   )
 }
